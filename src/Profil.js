7880eb212d9dbe534bc08200bee74088606a1269
sokmen4083 <sokmen4083@gmail.com>
2020-07-08 23:54:34 +0200

    new

--- src/Profil.js
+++ src/Profil.js
@@ -9,6 +9,12 @@ export class Profil extends Component {
       <div>
         <h1>REGISTRATION FORM</h1>
         <p>Welcome.</p>
+        <p>NAME</p>
+        <input type="text" placeholder="please enter your name"></input>
+        <p>SURNAME</p>
+        <input type="text" placeholder="please enter your Ssurname"></input>
+        <p>EMAIL</p>
+        <input type="email" placeholder="please enter your Email"></input>
       </div>
     );
   }

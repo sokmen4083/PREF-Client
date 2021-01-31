1370ae0eb148e45e1423350665a2f7584804db1f
Mehmet Sökmen <sokmen4083@gmail.com>
2021-01-31 21:19:21 +0100

    Site has done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'
 import { Family } from './Dashboard/FamilyUnion';
 import { PrefDocuments } from './Dashboard/PrefDocuments';
 import logo from './logom.png';
@@ -15,7 +16,7 @@ import { CampChange } from './Dashboard/CampChange';
 function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <Container>
           <Row>
             <Col>

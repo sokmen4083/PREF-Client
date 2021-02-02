716d1135570a78ceed43e9b4a92de609ddccf0d1
Mehmet Sökmen <sokmen4083@gmail.com>
2021-02-03 00:50:39 +0100

    Updates

---
+++ precache-manifest.e4cafb10faf153319fc8ae69e39cef13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee95d6a5138d42388fa92dede2e4a5b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ccf05fc476f0000418e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fffbb146fd5704380d70",
+    "url": "/static/css/main.ebd57477.chunk.css"
+  },
+  {
+    "revision": "3ccf05fc476f0000418e",
+    "url": "/static/js/2.d7f951f7.chunk.js"
+  },
+  {
+    "revision": "b787d5510093951f43ddb639d3cb0273",
+    "url": "/static/js/2.d7f951f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fffbb146fd5704380d70",
+    "url": "/static/js/main.3304fb77.chunk.js"
+  },
+  {
+    "revision": "6f2a7152b3a4473f2533",
+    "url": "/static/js/runtime-main.dbdb7264.js"
+  },
+  {
+    "revision": "18c3888f364246a853eb25e459360731",
+    "url": "/static/media/change.18c3888f.svg"
+  }
+]);
